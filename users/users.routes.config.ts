1b4e99c98683b1c295d32ee3fe4e5d94e1fb20c2
bthayer1990 <bthayer1990@gmail.com>
2022-01-18 12:09:06 -0600

    authorization checkpoint

--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -4,6 +4,9 @@ import UsersMiddleware from './middleware/users.middleware';
 import express from 'express';
 import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 import { body } from 'express-validator';
+import jwtMiddleware from '../auth/middleware/jwt.middleware';
+import permissionMiddleware from '../common/middleware/common.permission.middleware';
+import { PermissionFlag } from '../common/middleware/common.permissionflag.enum';
 
 export class UsersRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
@@ -13,7 +16,13 @@ export class UsersRoutes extends CommonRoutesConfig {
     configureRoutes(): express.Application {
         this.app
             .route(`/users`)
-            .get(UsersController.listUsers)
+            .get(
+                jwtMiddleware.validJWTNeeded,
+                permissionMiddleware.permissionFlagRequired(
+                    PermissionFlag.ADMIN_PERMISSION
+                ),
+                UsersController.listUsers
+            )
             .post(
                 body('email').isEmail(),
                 body('password').isLength({ min: 5 }).withMessage('Must include password (5+ characters)'),
@@ -25,7 +34,11 @@ export class UsersRoutes extends CommonRoutesConfig {
         this.app.param(`userId`, UsersMiddleware.extractUserId);
         this.app
             .route(`/users/:userId`)
-            .all(UsersMiddleware.validateUserExists)
+            .all(
+                UsersMiddleware.validateUserExists,
+                jwtMiddleware.validJWTNeeded,
+                permissionMiddleware.onlySameUserOrAdminCanDoThisAction
+            )
             .get(UsersController.getUserById)
             .delete(UsersController.removeUser);
 
@@ -37,6 +50,10 @@ export class UsersRoutes extends CommonRoutesConfig {
             body('permissionFlags').isInt(),
             BodyValidationMiddleware.verifyBodyFieldsErrors,
             UsersMiddleware.validateSameEmailBelongToSameUser,
+            UsersMiddleware.userCantChangePermission,
+            permissionMiddleware.permissionFlagRequired(
+                PermissionFlag.PAID_PERMISSION
+            ),
             UsersController.put,
         ]);
 
@@ -48,6 +65,10 @@ export class UsersRoutes extends CommonRoutesConfig {
             body('permissionFlags').isInt().optional(),
             BodyValidationMiddleware.verifyBodyFieldsErrors,
             UsersMiddleware.validatePatchEmail,
+            UsersMiddleware.userCantChangePermission,
+            permissionMiddleware.permissionFlagRequired(
+                PermissionFlag.PAID_PERMISSION
+            ),
             UsersController.patch,
         ]);
 
